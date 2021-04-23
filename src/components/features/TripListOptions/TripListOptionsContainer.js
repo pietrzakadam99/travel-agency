f35d4f2de3b61d0efc41adad84527ca3620f3e43
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-04-23 18:46:14 +0200

    correct

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase, changeDuration, changeTag} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, removeTag, addTag} from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -11,8 +11,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  addTag: payload => dispatch(addTag(payload)),
   changeDuration: payload => dispatch(changeDuration(payload)),
-  changeTag: payload => dispatch(changeTag(payload)),
+  removeTag: payload => dispatch(removeTag(payload)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
