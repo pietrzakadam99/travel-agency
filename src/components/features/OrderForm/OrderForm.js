857a0f1202df0cc6df1f196fcd818c2d366880f2
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-05-08 13:24:31 +0200

    Added API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,38 @@ import {Col, Row} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost, tripDetails) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    ...tripDetails,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = props => (
   <Row>
@@ -14,6 +46,7 @@ const OrderForm = props => (
     )}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
+      <Button onClick={() => sendOrder(props.options, props.tripCost, props.tripDetails )}>Order now!</Button>
     </Col>
   </Row>
 );
@@ -23,6 +56,7 @@ OrderForm.propTypes = {
   options: PropTypes.any,
   setOrderOption: PropTypes.func,
   name: PropTypes.string,
+  tripDetails: PropTypes.object,
 };
 
 export default OrderForm;
